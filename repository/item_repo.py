0c32cde58e14f2dd9c320775360dcb43517e4c59
0CreepySmile0 <yutivich2005@gmail.com>
2026-04-11 22:09:12 +0700

    feat(data-repository): Implement `ItemRepo`

---
+++ repository/item_repo.py
@@ -0,0 +1,53 @@
+from db import DBConnector
+
+class ItemRepo:
+
+    def __init__(self, db: DBConnector):
+        self.db = db
+
+    def create_item(self, item_name, description, price):
+        query = """
+        INSERT INTO items (item_name, description, price)
+        VALUES (?, ?, ?)
+        """
+        self.db.execute(query, (item_name, description, price))
+
+    def get_item_by_id(self, item_id):
+        query = """
+        SELECT * FROM items
+        WHERE id = ?
+        """
+        return self.db.fetchone(query, (item_id,))
+
+    def get_all_items(self):
+        query = "SELECT * FROM items"
+        return self.db.fetchall(query)
+
+    def update_item(self, item_id, item_name=None, description=None, price=None):
+        fields = []
+        values = []
+
+        if item_name is not None:
+            fields.append("item_name = ?")
+            values.append(item_name)
+
+        if description is not None:
+            fields.append("description = ?")
+            values.append(description)
+
+        if price is not None:
+            fields.append("price = ?")
+            values.append(price)
+
+        if not fields:
+            return
+
+        query = f"""
+            UPDATE items
+            SET {', '.join(fields)}
+            WHERE item_id = ?
+        """
+
+        self.db.execute(query, values)
+
+        
