c77fec2e4c1c15ec10b63b135b59fd7b635f9c6c
0CreepySmile0 <yutivich2005@gmail.com>
2026-04-13 09:18:24 +0700

    feat(waiter_service): Implement `WaiterService`

---
+++ backend/services/waiter_service.py
@@ -0,0 +1,16 @@
+from backend.repository.order_repo import OrderRepo, SERVED_STATUS, SERVING_STATUS, COOKING_STATUS
+
+class WaiterService:
+
+    def __init__(self, order_repo: OrderRepo):
+        self.order_repo = order_repo
+
+    def read_orders(self):
+        return self.order_repo.get_orders_by_status([COOKING_STATUS, SERVING_STATUS])
+
+    def serve_dish(self, order_id):
+        order = self.order_repo.get_order_by_id(order_id)
+        if order["status"] != SERVING_STATUS:
+            return False
+        self.order_repo.update_order_status(order_id, SERVED_STATUS)
+        return True
